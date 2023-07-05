d7ffa2161742774531146cf2f6c583dfadbe66e5
MauriBarrientos <mauribarrientos23@gmail.com>
2023-07-05 14:45:46 -0300

    Creacion de los controladores para el CRUD

--- controllers/reserva.controllers.js
+++ controllers/reserva.controllers.js
@@ -1,13 +1,153 @@
 const ctrlReservas = {};
 
-// ==========================================
-//         Rutas para CRUD de reservas
-// ==========================================
+const ctrlReserva = {};
+const Reserva = require('../models/Reserva');
 
-// Obtener todas las reservas
-// Obtener una reserva
-// Crear una reserva
-// Actualizar una reserva
-// Eliminar una reserva de forma lógica
+// Controlador para obtener todas las reservas
+ctrlReserva.obtenerReservas = async (req, res) => {
+    try {
+        const reservas = await Reserva.findAll({
+            where: {
+                estado: true,
+            }
+        });
+
+        if (!reservas || reservas.length === 0) {
+            throw ({
+                status: 404,
+                message: 'No hay reservas registradas'
+            })
+        }
+        console.log(reservas)
+
+        return res.json(reservas);
+    } catch (error) {
+        console.log('Error de trycatch en obtener reservas')
+        console.log(error)
+        return res.status(error.status || 500).json({
+            message: error.message || 'Error interno del servidor'
+        });
+    }
+}
+
+// Controlador para obtener una sola reserva
+ctrlReserva.obtenerReserva = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const reserva = await Reserva.findOne({
+            where: {
+                id,
+                estado: true
+            }
+        });
+
+        if (!reserva) {
+            throw ({
+                status: 404,
+                message: 'No existe la reserva'
+            })
+        }
+    
+        return res.json(reserva);
+
+    } catch (error) {
+        return res.status(error.status || 500).json(error.message || 'Error interno del servidor');
+    }
+}
+
+// Controlador para crear una reserva
+ctrlReserva.crearReserva = async (req, res) => {
+    const { nombre,apellido,fechaEntrada,fechaSalida,numero} = req.body;
+    
+    try {
+        const reserva = await Reserva.create({
+            nombre,
+            apellido,
+            fechaEntrada,
+            fechaSalida,
+            numero
+        });
+
+        if (!reserva) {
+            throw ({
+                status: 400,
+                message: 'No se pudo crear la reserva'
+            })
+        }
+
+        return res.json(reserva);
+    } catch (error) {
+        console.log(error);
+        return res.status(error.status || 500).json(error.message || 'Error interno del servidor');
+    }
+}
+
+// Controlador para actualizar o editar una reserva
+ctrlReserva.actualizarReserva = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, fechaEntrada, fechaSalida, numero } = req.body;
+    
+    try {
+        const reservaActualizada = await Reserva.update({
+            nombre,
+            apellido,
+            fechaEntrada,
+            fechaSalida,
+            numero,
+        }, {
+            where: {
+                id,
+                estado: true
+            }
+        });
+
+        if (!reservaActualizada) {
+            throw ({
+                status: 400,
+                message: 'No se pudo actualizar la reserva'
+            })
+        }
+
+        return res.json({
+            message: 'reserva actualizada correctamente',
+            reservaActualizada
+            
+        });
+    } catch (error) {
+        return res.status(error.status || 500).json(error.message || 'Error interno del servidor');
+    }
+}
+
+
+// Controlador para eliminar una reserva.
+
+ctrlReserva.eliminarReserva = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const reservaEliminada = await Reserva.update({
+            estado: false
+        }, {
+            where: {
+                id,
+                estado: true
+            }
+        });
+
+        if (!reservaEliminada) {
+            throw ({
+                status: 400,
+                message: 'No se pudo eliminar la reserva'
+            })
+        }
+
+        return res.json({reservaEliminada, message: 'reserva eliminada correctamente' });
+    } catch (error) {
+        return res.status(error.status || 500).json(error.message || 'Error interno del servidor');
+    }
+}
+
+module.exports = ctrlReserva;
 
 module.exports = ctrlReservas;
